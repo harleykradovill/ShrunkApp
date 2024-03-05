4579acc6b7cf1f8564cd7a47e48692f3fe66ddcd
Harley Kradovill <harleykradovill@gmail.com>
2024-03-05 17:32:40 -0600

    customization of tab bars and headers

--- screens/Scan.tsx
+++ screens/Scan.tsx
@@ -7,28 +7,7 @@ import { useState } from 'react';
 const Scan = () => {
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
-  if (permission === 'denied') {
-    return (
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: COLORS.white,
-      }}>
-        <Text>Camera permission is required to use this feature.</Text>
-        <TouchableOpacity
-          onPress={requestPermission}
-          style={{
-            padding: 10,
-            backgroundColor: COLORS.black,
-            borderRadius: 5,
-            marginTop: 10,
-          }}>
-          <Text style={{ color: COLORS.white }}>Request Permission</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+
   return (
     <View style={{
       flex: 1,
