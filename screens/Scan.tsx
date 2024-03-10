25fbf67ecb6e66a349f0a0767a3c510f7486b144
Harley Kradovill <harleykradovill@gmail.com>
2024-03-09 21:05:50 -0600

    updates

--- screens/Scan.tsx
+++ screens/Scan.tsx
@@ -1,23 +1,8 @@
 import { View, Button, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import COLORS from "../constants/colors";
-import { BarCodeScanner } from 'expo-barcode-scanner';
 
 const Scan = () => {
-  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-  const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not yet scanned');
-  const askForCameraPermission = () => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }
-
-  useEffect(() => {
-    askForCameraPermission();
-  }, []);
-
   return (
    <View style={{
      flex: 1,
